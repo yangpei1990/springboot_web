b6371b2af721a12aa3b77442d8e5688fcc07f7da
yangpei1990 <593378073@qq.com>
2020-06-13 00:26:53 +0800

    test commit git

---
+++ src/main/java/com/test/spring/controller/LoginController.java
@@ -0,0 +1,16 @@
+package com.test.spring.controller;
+
+
+import com.test.springboot.spring_web.service.LoginService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class LoginController {
+    @Autowired
+    public LoginService loginService;
+    public String login(){
+       //  Users user =  loginService.login();
+        return "SUCCESS";
+    }
+}
